21793832eedb0127e93623de61d1b048d0209d3d
fang08.li <fang08.li@vipshop.com>
2017-06-06 14:31:48 +0800

    ff

---
+++ src/main/java/com/vip/eureka/main/Starter.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.vip.eureka.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author fang08.li
+ *
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Starter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		
+		SpringApplication.run(Starter.class, args);
+	}
+
+}
